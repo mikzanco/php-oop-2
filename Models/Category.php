b3a0b422bcdc061390aa1121a1d039bc502526e7
mikzanco <mikyzancolo97@gmail.com>
2022-12-12 18:31:16 +0100

    fix category

--- Models/Category.php
+++ Models/Category.php
@@ -9,4 +9,12 @@ class Category{
         $this->name = $_name;
         $this->icon = $_icon;
     }
+
+    public function getIcon(){
+        if($this->name === 'Cane'){
+            $this->icon = "https://image.shutterstock.com/image-vector/pet-dog-line-icon-outline-260nw-529796020.jpg";
+        }elseif($this->name === 'Cat'){
+            $this->icon = "https://image.shutterstock.com/image-vector/cat-line-icon-outline-vector-260nw-726803200.jpg";
+        }
+    }
 }
--- index.php
+++ index.php
@@ -1,15 +1,15 @@
 <?php
 
-require __DIR__ . '/database/db.php';
+require_once __DIR__ . '/database/db.php';
 
 
 // $bone = new Toy(new Category('Cani', ), 94502, 'Osso', 'Ossi ossa', 2, true, );
 
 // $bone->setImg("https://shop-cdn-m.mediazs.com/bilder/nylabone/dental/osso/gioco/per/cuccioli/9/400/186512_pla_interpet_nylabone_welpen_dental_kauknochen_1stck_hs_02_9.jpg");
 
-include __DIR__ . '/Views/layout/head.php';
-include __DIR__ . '/Views/layout/header.php';
-include __DIR__ . '/Views/layout/main.php';
-include __DIR__ . '/Views/layout/footer.php';
+include_once __DIR__ . '/Views/layout/head.php';
+include_once __DIR__ . '/Views/layout/header.php';
+include_once __DIR__ . '/Views/layout/main.php';
+include_once __DIR__ . '/Views/layout/footer.php';
 ?>
 
