6e7836aad676ff6b0abd8d5d23545df28d3838a7
mikzanco <mikyzancolo97@gmail.com>
2022-12-12 16:37:34 +0100

    include all file in index.php with require

--- index.php
+++ index.php
@@ -1,5 +1,11 @@
 <?php
 
+require __DIR__ . '/Models/Category.php';
+require __DIR__ . '/Models/Product.php';
+require __DIR__ . '/Models/Accessory.php';
+require __DIR__ . '/Models/Food.php';
+require __DIR__ . '/Models/Toy.php';
+
 
 
 
