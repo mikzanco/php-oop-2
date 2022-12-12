cbfd9262687915fe049dbed0c18f97b8acec1fdd
mikzanco <mikyzancolo97@gmail.com>
2022-12-12 18:23:01 +0100

    fix require and inlclude with once

--- database/db.php
+++ database/db.php
@@ -1,7 +1,7 @@
 <?php
 
-require __DIR__ . '/Models/Category.php';
-require __DIR__ . '/Models/Product.php';
-require __DIR__ . '/Models/Accessory.php';
-require __DIR__ . '/Models/Food.php';
-require __DIR__ . '/Models/Toy.php';
+require_once __DIR__ . '/Models/Category.php';
+require_once __DIR__ . '/Models/Product.php';
+require_once __DIR__ . '/Models/Accessory.php';
+require_once __DIR__ . '/Models/Food.php';
+require_once __DIR__ . '/Models/Toy.php';
